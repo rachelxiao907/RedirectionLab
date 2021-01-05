1d4b6850fe91fb7f15f112abe3ea6806e0f04503
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-04 19:42:48 -0500

    Finished pigLatin method

--- PigLatin.java
+++ PigLatin.java
@@ -5,11 +5,17 @@ public class PigLatin {
     while (in.hasNextLine()) {
       Scanner line = new Scanner(in.nextLine());
       while (line.hasNext()) {
-        System.out.print(pigLatinSimple(line.next()));
+        System.out.print(pigLatin(line.next()));
         System.out.print(" ");
       }
       System.out.println();
     }
+    // Testing
+    // System.out.println(pigLatin("a"));
+    // System.out.println(pigLatin("check"));
+    // System.out.println(pigLatin("skee"));
+    // System.out.println(pigLatin("emu"));
+    // System.out.println(pigLatin("grade"));
   }
 
   public static String pigLatinSimple(String s) {
@@ -18,6 +24,22 @@ public class PigLatin {
       return s + "hay";
     }
     return s.substring(1) + s.substring(0,1) + "ay";
+  }
+
+  public static String pigLatin(String s) {
+    if (s.length() == 1) return pigLatinSimple(s);
+    s = s.toLowerCase();
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
+                          "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc",
+                          "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th",
+                          "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < digraphs.length; i++) {
+      if (s.substring(0,2).equals(digraphs[i])) {
+        i = digraphs.length;
+        return s.substring(2) + s.substring(0,2) + "ay";
+      }
+    }
+    return pigLatinSimple(s);
   }
 
 }
